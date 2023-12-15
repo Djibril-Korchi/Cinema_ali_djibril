7981a75dbf51893bd3f9615a9d3f511d9f191074
base <email@example.com>
2023-12-15 11:57:04 +0100

    création de la fonction ajout de film

--- Cinema doc/AjtFilm.php
+++ Cinema doc/AjtFilm.php
@@ -1,1 +1,18 @@
 <?php
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=dki_cinema;charset=utf8', 'root', '');
+}
+catch (Exception $e){
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+
+$req = $bdd->prepare('INSERT INTO film (Titre, Temps, Auteur, synopsie,Affiche) VALUES(:Titre, :Temps, :Auteur, :synopsie,:Affiche)');
+$req->execute(array(
+    'Titre' => $_POST['Titre'],
+    'Temps'=>$_POST['Temps'],
+    'Auteur'=>$_POST['Auteur'],
+    'synopsie'=>$_POST['Synopsie'],
+    'Affiche'=>$_POST['Affiche']
+));
