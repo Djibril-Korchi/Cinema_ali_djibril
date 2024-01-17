9366ab37fe208fb40d3e6aec3de24f934891c1a0
sebbane nassima <d.korchi@lprs.fr>
2024-01-17 18:45:14 +0100

    FEAT:Modifier

--- Cinema doc/Suprimer_salle.php
+++ Cinema doc/Suprimer_salle.php
@@ -1,7 +1,7 @@
 <?php
 $id=$_POST['suprimer'];
 $bdd=new PDO('mysql:host=localhost;dbname=dki_cinema;charset=utf8', 'root', '');
-$modif=$bdd->prepare("DELETE FROM sallecinema WHERE id_film = :id_film");
+$modif=$bdd->prepare("DELETE FROM sallecinema WHERE id_salle = :id_film");
 
 $modif->execute(array(
     'id_film'=>$id
---
+++ Cinema doc/forulaire_de_modiffication_salle.php
@@ -0,0 +1,23 @@
+<form method="post" action="Modifier.php">
+    <table>
+        <?php
+        $id = $_POST['modif'];
+        $bdd = new PDO('mysql:host=localhost;dbname=dki_crudphp;charset=utf8', 'root', '');
+        $reponse = $bdd->prepare('SELECT * FROM sallecinema WHERE id_salle = :id');
+        $reponse->execute(array(
+            'id' => $id
+        ));
+        $donne = $reponse->fetchAll();
+        foreach ($donne as $element) {
+            echo "
+                <tr><td>L'id de la salle :</td><td><input type='text' name='id_salle' value=" . $element['id_salle'] . " readonly></td></tr>
+                <tr><td>Le Type de la salle :</td><td><input type='text' name='typesalle' value=" . $element['TypeSalle'] . "></td></tr>
+                <tr><td>Le Nombre de Place de la salle :</td><td><input type='text' name='nbplace' value=" . $element['NombrePlace'] . "></td></tr>
+                <tr><td>Le Film diffusé dans la salle :</td><td><input type='text' name='ref_film' value=" . $element[''] . "></td></tr>
+                <tr><td><input type='submit' value='Valider'></td></tr>
+            ";
+        }
+        ?>
+    </table>
+</form>
+
